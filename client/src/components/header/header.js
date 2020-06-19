51175fb96f2d7a111dd7aeb18e856083a35dfd55
Alexandru Damian <damian.alecsandru@gmail.com>
2020-06-19 10:57:43 +0300

    added header component

---
+++ client/src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+export class header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h2>Where in the world?</h2>
+            </div>
+        )
+    }
+}
+
+export default header
