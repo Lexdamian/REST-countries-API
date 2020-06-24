835b56e4db31bf875fab5a3c5ce5ae8b0450935c
Alexandru Damian <damian.alecsandru@gmail.com>
2020-06-24 10:52:48 +0300

    country enhancements

--- client/src/components/countries/country.js
+++ client/src/components/countries/country.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './country.css'
 
-export class Country extends Component {
-    render() {
-        return (
-            <Link to={`/country/${this.props.country.name}`} target='_blank' key={this.props.country.numericCode} style={{textDecoration: 'none', color: 'black'}}>
-                <div className="countryCard">
-                    <div className="imgContainer">
-                        <img alt="country flag" src={this.props.country.flag} />
-                    </div>
-                    <div className="countryInfo">
-                        <h3>{this.props.country.name}</h3>
-                        <p>Population: {this.props.country.population}</p>
-                        <p>Region: {this.props.country.region} </p>
-                        <p>Capital: {this.props.country.capital} </p>
-                    </div>
+const Country = ({ country }) => {
+
+    return (
+        <Link to={`/country/${country.name}`} target='_blank' key={country.numericCode} style={{ textDecoration: 'none', color: 'black' }}>
+            <div className="countryCard">
+                <div className="imgContainer">
+                    <img alt="country flag" src={country.flag} />
                 </div>
-            </Link>
-        )
-    }
+                <div className="countryInfo">
+                    <h3>{country.name}</h3>
+                    <p>Population: {country.population}</p>
+                    <p>Region: {country.region} </p>
+                    <p>Capital: {country.capital} </p>
+                </div>
+            </div>
+        </Link>
+    )
+
 }
 
 export default Country;
