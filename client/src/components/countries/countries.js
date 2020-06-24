85bf333f444fcbbd8770df46d8b4a81ed513968e
Alexandru Damian <damian.alecsandru@gmail.com>
2020-06-24 10:39:54 +0300

    countries enhancements

--- client/src/components/countries/countries.js
+++ client/src/components/countries/countries.js
@@ -2,17 +2,14 @@ import React from 'react';
 import Country from './country';
 
 
-class Countries extends React.Component {
-
-    render() {
-        return (
-            <div className='countriesContainer'>
-                {this.props.countries.map((country) => (
-                    <Country country={country} key={country.numericCode}/>
-                ))}
-            </div>
-        )
-    }
+const Countries = ({countries}) => {
+    return (
+        <div className='countriesContainer'>
+            {countries.map((country) => (
+                <Country country={country} key={country.numericCode} />
+            ))}
+        </div>
+    )
 }
 
 export default Countries;
