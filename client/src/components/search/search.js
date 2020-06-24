14f272b99cc7f8de83ce5a53dd505c61cb4eb916
Alexandru Damian <damian.alecsandru@gmail.com>
2020-06-24 10:49:00 +0300

    search enhancements

--- client/src/components/search/search.js
+++ client/src/components/search/search.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './search.css'
 
-class Search extends Component {
+const Search = (props) => {
 
-  handleInputChange = () => {
+  const handleInputChange = () => {
 
     let name = document.getElementById('search').value;
 
@@ -12,7 +12,7 @@ class Search extends Component {
         .then(res => res.json())
         .then(data => {
           if (data.length > 0) {
-            this.props.updateCountries(data)
+            props.updateCountries(data)
           } else {
             return ('No countries found')
           }
@@ -21,24 +21,21 @@ class Search extends Component {
     } else if (name.length === 0) {
       fetch('/countries')
         .then(res => res.json())
-        .then(data => this.props.updateCountries(data))
+        .then(data => props.updateCountries(data))
         .catch(err => console.log(err))
     }
   }
 
-  render() {
-    return (
-      <form>
-        <input
-          id='search'
-          placeholder="Search for..."
-          ref={input => this.search = input}
-          onChange={this.handleInputChange}
-          className="searchBox"
-        />
-      </form>
-    )
-  }
+  return (
+    <form>
+      <input
+        id='search'
+        placeholder="Search for..."
+        onChange={handleInputChange}
+        className="searchBox"
+      />
+    </form>
+  )
 }
 
 export default Search
